9754a51a1a8f029a295c6d8898a2869f5b93e459
FaneNohman <kingofthepixels1@gmail.com>
2023-03-31 16:55:13 +0600

    first commit

---
+++ WebApplication2/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace CORE_MVC_EXAM.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ WebApplication2/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string YearOfProduction { get; set; }
+        public string BodyType { get; set; }
+        public double Price { get; set; }
+        public double EngineVolume { get; set; }
+        public bool CustomsClear { get; set; }
+        public string Comment { get; set; }
+    }
+}
